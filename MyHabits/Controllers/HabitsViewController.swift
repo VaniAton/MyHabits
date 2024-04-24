cd782e2474c3102f62a9217851d8d233504c2918
VanilarAnton <koryakinanton3@gmail.com>
2024-04-25 00:24:51 +0300

    add label for HabitsVC

--- MyHabits/Controllers/HabitsViewController.swift
+++ MyHabits/Controllers/HabitsViewController.swift
@@ -2,7 +2,16 @@ import UIKit
 
 class HabitsViewController: UIViewController {
     
-    var titlePost: String = "Сегодня"
+    private lazy var todayLabel: UILabel = {
+        let todayLabel = UILabel()
+        todayLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayLabel.text = "Сегодня"
+        todayLabel.textColor = UIColor("#000000")
+        todayLabel.highlightedTextColor = UIColor("#000000")
+        todayLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        
+        return todayLabel
+    }()
     
     private lazy var openButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openInfo))
     
@@ -11,11 +20,23 @@ class HabitsViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = openButton
         view.backgroundColor = UIColor("#FFFFFF")
+        view.addSubview(todayLabel)
+        setupConstraintsForHabitVC()
     }
     
     @objc func openInfo(_ sender: UIButton) {
         _ = InfoViewController()
+    }
+    
+    private func setupConstraintsForHabitVC() {
         
+        NSLayoutConstraint.activate([
         
+            todayLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
+            todayLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            todayLabel.widthAnchor.constraint(equalToConstant: 141)
+            
+        
+        ])
     }
 }
